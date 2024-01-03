106fc933b034368b50c15d52347444ff57bc239a
Mohamad khairil azizee bin mohamed @ ayub <khairil.mohamed@mdec.com.my>
2024-01-03 11:04:15 +0800

    add page

---
+++ app/dashboard/layout.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Sidebar from '../components/Sidebar/Sidebar'
+import Navbar from '../components/Navbar/Navbar'
+import Footer from '../components/Footer/Footer'
+
+const DashboardLayout = ({ children }) => {
+    return (
+        <div className="flex">
+            <div className="w-1/5 bg-[color:var(--bgSoft)] p-[20px]">
+                <Sidebar />
+            </div>
+            <div className="w-4/5 p-[20px]">
+                <Navbar />
+                {children}
+                <Footer />
+            </div>
+        </div>
+    )
+}
+
+export default DashboardLayout
--- app/register/register.jsx
+++ app/register/register.jsx
@@ -1,58 +1,63 @@
 "use client"
-import Link from 'next/link'
 import React, { useState } from 'react'
+import Link from 'next/link'
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import { useForm } from 'react-hook-form'
 import { useRouter } from 'next/navigation'
-import { NextSeo } from 'next-seo'
 
-const Login = () => {
+
+const RegisterLogic = () => {
+
     const { register, handleSubmit } = useForm();
-    const [error, setError] = useState("")
     const router = useRouter();
-
     const supabase = createClientComponentClient();
+    const [error, setError] = useState("")
+
 
     const onSubmit = async (data) => {
         try {
-            const { data: { user }, error } = await supabase.auth.signInWithPassword({
+            const { data: { user }, error } = await supabase.auth.signUp({
                 email: data.email,
-                password: data.password
+                password: data.password,
             })
-            if (user) router.push("dashboard")
-            if (error) setError(error);
+            if (user) {
+                router.push("login")
+            }
+            if (error) setError(error)
         } catch (error) {
-            return console.debug(error)
+            console.log(error)
         }
-    }
 
+    }
 
     return (
         <>
-            <NextSeo
-                title="Log In"
-                description="A short description goes here."
-            />
             <div className="flex flex-col justify-center items-center h-screen gap-5">
                 {error && (<>
                     <div className="py-2 px-3 bg-red-600 text-white text-center uppercase font-medium">{error.message}</div>
                 </>)}
-                <div>LOG IN YOUR ACCOUNT</div>
+                <div>REGISTER YOUR ACCOUNT</div>
                 <form className="bg-black" onSubmit={handleSubmit(onSubmit)}>
+                    <div className="flex flex-col mb-5">
+                        <input type="text" placeholder="Name" className="text-black py-2 px-4 rounded-md" {...register("name", { required: true })} />
+                    </div>
                     <div className="flex flex-col mb-5">
                         <input type="email" placeholder="Email" className="text-black py-2 px-4 rounded-md" {...register("email", { required: true })} />
                     </div>
                     <div className="flex flex-col mb-5">
                         <input type="password" placeholder="Password" className="text-black py-2 px-4 rounded-md" {...register("password", { required: true })} />
                     </div>
+                    <div className="flex flex-col mb-5">
+                        <input type="password" placeholder="Confirm Password" className="text-black py-2 px-4 rounded-md" {...register("confirm_password", { required: true })} />
+                    </div>
                     <div className="flex flex-row justify-center gap-5 mb-5">
-                        <Link href="register" className="hover:underline flex items-center">
+                        <Link href="login" className="hover:underline flex items-center">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" fill="none" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                                 <path d="M19 12H5M12 19l-7-7 7-7" />
                             </svg>
-                            <div>Register</div>
+                            <div>Login</div>
                         </Link>
-                        <button className="hover:underline px-3 py-2 bg-blue-500 rounded-md">Login</button>
+                        <button className="hover:underline px-3 py-2 bg-blue-500 rounded-md">Register</button>
                     </div>
                 </form>
             </div>
@@ -60,4 +65,4 @@ const Login = () => {
     )
 }
 
-export default Login
+export default RegisterLogic
